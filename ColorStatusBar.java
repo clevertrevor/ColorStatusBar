dfd9fe63317441ca030fc6d69f83b6470ac3c9e6
Trevor <trevornemanic@gmail.com>
2015-06-14 12:54:33 -0700

    Create ColorStatusBar.java

---
+++ ColorStatusBar.java
@@ -0,0 +1,31 @@
+import android.app.Activity;
+import android.os.Build;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class ColorStatusBar {
+
+    private static Activity activity;
+
+    public static void setStatusBarColor(Activity act, View statusBar, int color) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            activity = act;
+            Window w = activity.getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            statusBar.getLayoutParams().height = getStatusBarHeight();
+            statusBar.setBackgroundColor(color);
+        }
+    }
+
+    private static int getStatusBarHeight() {
+        int result = 0;
+        int resourceId = activity.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = activity.getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }
+
+}
